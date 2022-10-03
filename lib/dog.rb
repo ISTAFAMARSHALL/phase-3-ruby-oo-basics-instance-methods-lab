30f5bf2b0c212f612cf82d4cf8a6a9d2c5127846
+ <+>
2022-10-03 12:27:15 -0400

    commit tree 3f59be98c5d67186e5bbf6e716391e92a30ed442

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog
+    # Class body
+  
+    # Instance Method Definition
+    def bark
+      puts "Woof!"
+      def sit
+        puts "The Dog is sitting"
+      end
+    end
+  end
+
+fido = Dog.new
+fido.bark
+
+snoopy = Dog.new
+snoopy.bark
+fido.sit
